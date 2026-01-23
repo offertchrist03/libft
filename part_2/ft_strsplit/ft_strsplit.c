41358a9989d667a3d0c34e5ea72e80d43b1fa2a4
Mamisoa Fitiavana Christ Offert Hendriniaina <mahendri@c1r9s7.42antananarivo.mg>
2026-01-23 08:10:04 +0300

    jan 23 08:10

--- part_2/ft_strsplit/ft_strsplit.c
+++ part_2/ft_strsplit/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: mahendri <mahendri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:42:00 by mahendri          #+#    #+#             */
-/*   Updated: 2026/01/22 22:30:29 by mahendri         ###   ########.fr       */
+/*   Updated: 2026/01/23 08:09:25 by mahendri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static size_t	ft_strlen(const char *s)
+void	*ft_memset(void *s, int c, unsigned int n)
+{
+	unsigned char	*buffer;
+	unsigned int	i;
+
+	buffer = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		buffer[i] = (char)c;
+		i++;
+	}
+	return (s);
+}
+
+size_t	ft_strlen(const char *s)
 {
 	unsigned int	i;
 
@@ -25,18 +40,25 @@ static size_t	ft_strlen(const char *s)
 }
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	j;
+	size_t	i;
+	size_t	j;
+	size_t	res;
 
+	if (size <= 0)
+		return (ft_strlen(src));
+	if (size > ft_strlen(dst))
+		res = ft_strlen(src) + ft_strlen(dst);
+	else
+		res = ft_strlen(src) + size;
 	i = ft_strlen(dst);
 	j = 0;
-	while (src[j] && j < size)
+	while (src[j] && ft_strlen(dst) < size - 1)
 	{
 		dst[i + j] = (char)src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	return (ft_strlen(dst));
+	return (res);
 }
 
 size_t	count_part(char const *s, char c)
@@ -56,50 +78,6 @@ size_t	count_part(char const *s, char c)
 		count++;
 	return (count);
 }
-size_t	*size_part(char const *s, char c)
-{
-	size_t	i;
-	size_t	j;
-	size_t	count;
-	size_t	*arr;
-	size_t	arr_i;
-
-	arr_i = 0;
-	arr = (size_t *)malloc(count_part(s, c) * sizeof(size_t));
-	if (!arr)
-	{
-		free(arr);
-		return (0);
-	}
-	i = 0;
-	j = 1;
-	count = 0;
-	while (s[i])
-	{
-		if (s[i] == c && !(s[i + 1] == c))
-		{
-			arr[count] = j - 1;
-			count++;
-			j = 0;
-		}
-		i++;
-		j++;
-	}
-	arr[count] = j - 1;
-	if (s[i] == '\0' && !(s[i - 1] == c))
-		count++;
-	return (arr);
-}
-
-int	split_part(char **arr_str, char const *s, char c)
-{
-	int	i;
-
-	i = 0;
-	(void)c;
-	ft_strlcat(arr_str[1], &s[i], 4);
-	return (1);
-}
 
 void	free_arr(char **arr, size_t size)
 {
@@ -114,43 +92,59 @@ void	free_arr(char **arr, size_t size)
 	free(arr);
 }
 
+void	do_split(char **buff_arr, const char *str, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	size_t	buff_count;
+
+	i = 0;
+	j = 0;
+	buff_count = count_part(str, c);
+	while (i < buff_count)
+	{
+		k = 0;
+		while (str[j])
+		{
+			if (str[j] == c)
+			{
+				j++;
+				buff_arr[i][k] = '\0';
+				break ;
+			}
+			buff_arr[i][k] = str[j];
+			j++;
+			k++;
+		}
+		i++;
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t	*arr_len;
-	char	**buffer_str;
+	char	**buff_arr;
+	size_t	buff_count;
 	size_t	i;
-	size_t	str_i;
-	size_t	count;
 
-	count = count_part(s, c);
-	buffer_str = (char **)malloc(count * sizeof(char *));
-	if (!buffer_str)
+	buff_count = count_part(s, c);
+	buff_arr = (char **)malloc(buff_count * sizeof(char *));
+	if (!buff_arr)
 	{
-		free(buffer_str);
+		free(buff_arr);
 		return (NULL);
 	}
 	i = 0;
-	arr_len = size_part(s, c);
-	printf("***\n");
-	while (arr_len[i] && i < count)
+	while (i < buff_count)
 	{
-		printf("[%zu]\n", arr_len[i]);
-		i++;
-	}
-	i = 0;
-	str_i = 0;
-	while (i < count)
-	{
-		buffer_str[i] = (char *)malloc(arr_len[i] * sizeof(char));
-		if (!buffer_str[i])
+		buff_arr[i] = (char *)malloc(100 * sizeof(char));
+		if (!buff_arr[i])
 		{
-			free(buffer_str[i]);
-			free(buffer_str);
+			free_arr(buff_arr, buff_count);
 			return (NULL);
 		}
 		i++;
 	}
-	printf(">> RES = %d\n", split_part(buffer_str, s, c));
-	free(arr_len);
-	return (buffer_str);
+	do_split(buff_arr, s, c);
+	return (buff_arr);
 }
--- part_2/ft_strsplit/main.c
+++ part_2/ft_strsplit/main.c
@@ -6,7 +6,7 @@
 /*   By: mahendri <mahendri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:38:13 by mahendri          #+#    #+#             */
-/*   Updated: 2026/01/22 22:07:34 by mahendri         ###   ########.fr       */
+/*   Updated: 2026/01/23 07:51:44 by mahendri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ void	free_arr(char **arr, size_t size);
 int	main(int argc, char **argv)
 {
 	char	**buffer_array;
+	size_t	i;
 
-	// int		i;
 	if (argc < 3)
 		return (1);
 	// buffer_array = ft_split(argv[1], argv[2][0]);
 	// printf("part= %zu\n", count_part(argv[1], argv[2][0]));
 	buffer_array = ft_split(argv[1], argv[2][0]);
-	// i = 0;
-	// while (i < 3 && buffer_array[i])
-	// {
-	// 	printf("%s\n", buffer_array[i]);
-	// 	i++;
-	// }
-	printf("%s", buffer_array[1]);
+	i = 0;
+	printf("\n");
+	while (i < count_part(argv[1], argv[2][0]))
+	{
+		printf("%s\n", buffer_array[i]);
+		i++;
+	}
 	free_arr(buffer_array, count_part(argv[1], argv[2][0]));
 	return (0);
 }
